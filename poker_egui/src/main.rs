f13e67310c722f6cae84041e5adf4ae2c3d829a0
zoe <zoe211@163.com>
2022-03-30 18:55:05 +0800

    结构调整

---
+++ poker_egui/src/main.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+use eframe::epi::{App, Frame};
+
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(MyApp::default()), options);
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _: &Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+
+        // Resize the native window to be just the size we need it to be:
+        // frame.set_window_size(ctx.used_size());
+    }
+
+    fn name(&self) -> &str {
+        "My egui Application"
+    }
+}
